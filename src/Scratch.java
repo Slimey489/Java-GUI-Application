2502246b484108eede4631ed6e822ab4f4dfab54
SarcasticSigma <sarcasticsigma@gmail.com>
2023-05-17 18:59:26 -0400

    Refactored into seperate files for sanity and found/fixed NPE

---
+++ src/Scratch.java
@@ -0,0 +1,192 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Optional;
+
+class Scratch {
+
+    /** @noinspection OptionalGetWithoutIsPresent*/
+    public static Class<?> findCallingClass() {
+        int classStack = 1;
+        StackWalker walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+        Optional<? extends Class<?>> caller = walker.walk(frames ->
+                frames.skip(classStack).findFirst().map(StackWalker.StackFrame::getDeclaringClass));
+        return caller.get();
+
+
+    }
+    /*What is T ?
+      T is a Type variable it replaces instances of an object
+    */
+    public static <T> ArrayList<T> removeDuplicates(ArrayList<T> list) {
+        ArrayList<T> duplicateRemover = new ArrayList<>();
+        for (T element : list) {
+            if (!duplicateRemover.contains(element)) {
+                duplicateRemover.add(element);
+            }
+        }
+        return duplicateRemover;
+    }
+
+    public static JFrame frameMain;
+    static Class<?> callingclass;
+    static Container contentpane;
+    static SpringLayout layout;
+    JTextField guessField;
+    JLabel enterGuessLabel;
+    static JLabel guessLabel;
+    JButton backButton;
+    String rawGuess, guess;
+    static String char2,letter;
+    static final String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+            "O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    static int letter_At_Index,letters_On_Row, column,row,total_Letters_Added;
+    ArrayList<String> underscoreWord;
+    //static File frameIsOpen;
+
+
+
+
+
+    public static int rowCalc() {
+
+        int leastRows = 7;
+
+        double unRoundedletterPerRow;
+        unRoundedletterPerRow = (((double) 26 / leastRows)-1);
+        MathContext rounded = new MathContext(1, RoundingMode.UP);
+        BigDecimal returnCorrectType = new BigDecimal(unRoundedletterPerRow,rounded);
+
+        return returnCorrectType.intValue();
+
+    }
+    /** @noinspection UnusedReturnValue*/
+    public JFrame GuessFrame(){
+
+        frameMain = new JFrame();
+        contentpane = frameMain.getContentPane();
+        layout = new SpringLayout();
+
+        //JButton button = new JButton("Enter");
+
+        guessField = new JTextField(1);
+        backButton = new JButton("New Word");
+        guessField.addActionListener(new action());
+        backButton.addActionListener(new action());
+        enterGuessLabel = new JLabel("Enter Guess:");
+
+
+        layout.putConstraint(SpringLayout.WEST, enterGuessLabel, 10, SpringLayout.WEST, contentpane);
+        layout.putConstraint(SpringLayout.NORTH, enterGuessLabel, 20, SpringLayout.NORTH, contentpane);
+        layout.putConstraint(SpringLayout.WEST, guessField, 80, SpringLayout.WEST, enterGuessLabel);
+        layout.putConstraint(SpringLayout.NORTH, guessField, 20, SpringLayout.NORTH, contentpane);
+        layout.putConstraint(SpringLayout.WEST, backButton, 20, SpringLayout.WEST, contentpane);
+        /*  The line below was causing the NPE. Not sure when/where guessLabel
+         is supposed to be defined. Using enterGuessLabel for now instead. */
+        layout.putConstraint(SpringLayout.NORTH, backButton, 20, SpringLayout.NORTH, enterGuessLabel);
+
+
+        frameMain.add(enterGuessLabel);
+        frameMain.add(backButton);
+        frameMain.add(guessField);
+        frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameMain.setSize(500, 400);
+        frameMain.setLayout(layout);
+
+        contentpane.setVisible(true);
+        frameMain.setVisible(true);
+
+        return(frameMain);
+    }
+
+
+    class action implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            char char1;
+
+            if (e.getSource() == guessField) {
+                rawGuess = guessField.getText();
+
+                if (rawGuess.length() == 0) {
+                    frameMain.setVisible(false);
+                    ErrorFrame.error = "Error Invalid Value For Guess";
+                    Scratch.callingclass = Scratch.findCallingClass();
+                    SwingUtilities.invokeLater(new ErrorHandler());
+                    return;
+                }
+                else {
+                    guess = rawGuess.toLowerCase();
+                }
+                char1 = guess.charAt(0);
+                String char2 = Character.toString(char1);
+                System.out.print(char2);
+                if (total_Letters_Added != 26){frameMain.add(place_Label());}
+                guessField.setText("");
+                frameMain.revalidate();
+            }
+            if (e.getSource() == backButton){
+                App.start_mainFrame_First = false;
+                App.class_is_mainframe = true;
+                SwingUtilities.invokeLater(new App());
+
+            }
+
+
+
+        }
+    }
+    static JLabel place_Label(){
+        int letters_Per_Row = rowCalc();
+        String char_Caps;
+        for(letter_At_Index = 0; letter_At_Index < alphabet.length; letter_At_Index++) {
+            // i--;
+            char_Caps = char2.toUpperCase();
+
+            if (char_Caps.equals(alphabet[letter_At_Index])) {
+             /*   if (placed_Letters.stream().findAny().isPresent()){
+                    /*
+                    frameMain.setVisible(false);
+                    ErrorFrame.error = "Error Invalid Value For Guess";
+                    Scratch.callingclass = Scratch.findCallingClass();
+                    SwingUtilities.invokeLater(new ErrorHandler());
+
+                     */
+                //     return null;
+                // }
+
+
+                if (letters_On_Row == letters_Per_Row) {
+                    row++;
+                    letters_On_Row = 0;
+                    column = 0;
+                }
+                letter = alphabet[letter_At_Index];
+                guessLabel = new JLabel(letter);
+                System.out.println(column * 20 + 20);
+                layout.putConstraint(SpringLayout.WEST, guessLabel, (column * 20 + 20), SpringLayout.WEST, contentpane);
+                layout.putConstraint(SpringLayout.NORTH, guessLabel, (row * 20 + 70), SpringLayout.NORTH, contentpane);
+                if (column == letters_Per_Row) {
+                    column = 0;
+                } else {
+                    column++;
+                }
+
+                letters_On_Row++;
+                total_Letters_Added++;
+                return (guessLabel);
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new App());
+
+    }
+    //f.add(button);
+}
---
+++ src/WordFrame.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
+
+class WordFrame extends JFrame {
+    public JFrame frameWord;
+    JLabel labelWord;
+    JButton enterWord;
+    JTextField wordField;
+    public Dimension originalSize, newSize;
+    SpringLayout layout2;
+    Container contentpane2;
+    public String word;
+    String[] wordArray;
+    public ArrayList<String> arrayWord;
+
+
+    public JFrame wordFrame() {
+
+        frameWord = new JFrame();
+        enterWord = new JButton("Enter");
+        contentpane2 = frameWord.getContentPane();
+        enterWord.addActionListener(new action());
+        frameWord.addComponentListener(new ResizeListener());
+        layout2 = new SpringLayout();
+        wordField = new JTextField(8);
+        labelWord = new JLabel("Enter a Word:");
+
+        layout2.putConstraint(SpringLayout.EAST, enterWord, -5, SpringLayout.EAST, contentpane2);
+        layout2.putConstraint(SpringLayout.NORTH, enterWord, 50, SpringLayout.NORTH, contentpane2);
+        layout2.putConstraint(SpringLayout.EAST, wordField, -5, SpringLayout.EAST, contentpane2);
+        layout2.putConstraint(SpringLayout.NORTH, wordField, 20, SpringLayout.NORTH, contentpane2);
+        layout2.putConstraint(SpringLayout.WEST, labelWord, 10, SpringLayout.WEST, contentpane2);
+        layout2.putConstraint(SpringLayout.NORTH, labelWord, 20, SpringLayout.NORTH, contentpane2);
+
+        enterWord.setSize(30, 20);
+        frameWord.add(labelWord);
+        frameWord.add(enterWord);
+        frameWord.add(wordField);
+        frameWord.setSize(210, 140);
+        originalSize = frameWord.getSize();
+        contentpane2.setLayout(layout2);
+
+        return frameWord;
+    }
+
+    class action implements ActionListener {
+        String wordLowerCase;
+
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == enterWord) {
+                word = wordField.getText();
+                if (Objects.equals(word, "")) {
+                    ErrorFrame.error = "No Word Entered.";
+                    Scratch.callingclass = Scratch.findCallingClass();
+                    SwingUtilities.invokeLater(new ErrorHandler());
+                    frameWord.dispatchEvent(new WindowEvent(frameWord, WindowEvent.WINDOW_CLOSING));
+                    return;
+                }
+                frameWord.dispatchEvent(new WindowEvent(frameWord, WindowEvent.WINDOW_CLOSING));
+
+                wordLowerCase = word.toLowerCase(Locale.ROOT);
+                wordArray = wordLowerCase.split("");
+
+                arrayWord = new ArrayList<>(Arrays.asList(wordArray));
+                Scratch classinstance = new Scratch();
+                classinstance.GuessFrame();
+            }
+        }
+    }
+
+    class ResizeListener implements ComponentListener {
+
+        public void componentHidden(ComponentEvent e) {
+        }
+
+        public void componentMoved(ComponentEvent e) {
+        }
+
+        public void componentShown(ComponentEvent e) {
+        }
+
+        public void componentResized(ComponentEvent e) {
+            newSize = e.getComponent().getBounds().getSize();
+            if (newSize != originalSize) {
+                frameWord.setSize(originalSize);
+
+            }
+        }
+    }
+}

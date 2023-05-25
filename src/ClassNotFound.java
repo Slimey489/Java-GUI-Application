97c5efa12336ce312df22cb8856886cef1164004
Slimey489 <destinyinvective@gmail.com>
2023-05-26 08:57:35 +1200

    Added Documentation for the custom ClassNotFound exception

--- src/ClassNotFound.java
+++ src/ClassNotFound.java
@@ -1,4 +1,9 @@
+/** This error triggers when the class returned by
+ * FindCallingClass is not one of the conditions in
+ * the if statement. No user should ever trigger this
+ * error.**/
 class ClassNotFound extends Exception
 {
-    public ClassNotFound() {}
+    public ClassNotFound() {
+    }
 }
